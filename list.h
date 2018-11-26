e74e585c1f5ea17ff5fe01278b20f4eef80f647e
Rice_Ball <34655446+tinyriceball@users.noreply.github.com>
2018-11-26 19:49:53 +0800

    Update list.h

--- list.h
+++ list.h
@@ -1,2 +1,26 @@
 #include <iostream>
 using namespace std;
+#define MAX
+class List
+{
+  public:
+    List();
+    ~List();
+    int Length();
+    void Insert();
+    void Delete();
+    int Get();
+
+  private:
+    int size;
+    int list[MAX];
+};
+List::List()
+{
+    size = 0;
+};
+List::~List(){};
+int List::Length()
+{
+    return size;
+}
