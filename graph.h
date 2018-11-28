a81898bdbda3074f3f9a4711ac1bad84a8f529d3
Rice_Ball <34655446+tinyriceball@users.noreply.github.com>
2018-11-28 21:23:37 +0800

    Add files via upload

--- graph.h
+++ graph.h
@@ -1,42 +1,43 @@
 #include <iostream>
+#include <fstream>
 #include "list.h"
 using namespace std;
 #define Vertex 100
-class Edge//边
+#define infinite 10000000
+class Edge //边
 {
-  public:
-    Edge();
-    ~Edge();
-    int Weight;//权重
-    int Row;//行
-    int Line;//列
+public:
+  Edge();
+  ~Edge();
+  int Weight; //权重
+  int Row;    //行
+  int Line;   //列
 };
 Edge::Edge()
 {
-    Weight = 0;
-    Row = 0;
-    Line = 0;
+  Weight = 0;
+  Row = 0;
+  Line = 0;
 };
 Edge::~Edge(){};
 class Graph
 {
-  public:
-    Graph();
-    ~Graph();
-    void Insert_vertex();//插入节点
-    void Delete_vertex();//删除节点
-    void Insert_edge();//插入边
-    void Delete_edge();//删除边
-    void Get_First();//获取第一个节点
-    void Get_Next();//获得下一个节点
-    void Dijkstra();//最短路径 
+public:
+  Graph();
+  ~Graph();
+  void Create(Graph *g);
+  void Insert_vertex(); //插入节点
+  void Delete_vertex(); //删除节点
+  void Insert_edge();   //插入边
+  void Delete_edge();   //删除边
+  int Get_First(Graph *g,int a); //获取第一个节点
+  int Get_Next(Graph *g, int a); //获得下一个节点
+  void Dijkstra(int v);      //最短路径
 
-  private:
-    List route;
-    int Edge_Num;
-    int Save_v[Vertex][Vertex];
+private:
+  List route;
+  int Edge_Num;
+  int Save_v[Vertex][Vertex];
 };
-Graph::Graph(){
-  Edge_Num=0;
-};
-Graph::~Graph(){};
+Graph *graph = new Graph;
+
