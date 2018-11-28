97a1dda031f74e34820d58c97fe243868ca35496
xuanxuan12345 <45167443+xuanxuan12345@users.noreply.github.com>
2018-11-28 19:10:33 +0800

    Add files via upload

--- LDelete.cpp
+++ LDelete.cpp
@@ -1,1 +1,25 @@
 #include "LDelete.h"
+#include <iostream>
+using namespace std;
+
+
+//删除链表中第i个位置的元素，并储存在x中
+List::Delete(List L,int i,int * x) {
+	if (Length(L) < 0) {
+		cout << "顺序表为空，删除操作失败" << endl;
+		return;
+	}
+	if (i<0 || i>Length(L) - 1) {
+		cout << "删除位置错误，操作失败" << endl;
+		return 0;
+	}else{
+		*x = L.list[i];
+		for (int j = i + 1; j <= Length(L) - 1; j++) {
+			L->list[j - 1] = L->list[j];
+			(L->size)--;
+		}
+	
+
+	}
+}
+
