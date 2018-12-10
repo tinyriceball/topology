f1bfaec0379791a6e5f6331e46482c356c84e25e
xuanxuan12345 <45167443+xuanxuan12345@users.noreply.github.com>
2018-12-10 18:53:45 +0800

    cout << "杨雯绚" << endl;

--- Gergodic.cpp
+++ Gergodic.cpp
@@ -3,11 +3,13 @@
 List * Graph::ergodic(List *h,int k) {
 	while (1) {
 		if (k == h->Name) {
+			cout << "杨雯绚" << endl;
 			return h;
 		}
 		else {
 			if (h->next == NULL) {
 				cout << "该路由器不存在！" << endl;
+				cout << "杨雯绚" << endl;
 				return NULL;
 			}
 			h = h->next;
